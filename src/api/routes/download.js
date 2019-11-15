7fec61b63ce8022a44eda2e2b1e50164d8d70d56
RahulDamineni <daminenirahul@gmail.com>
2019-11-14 23:28:08 -0800

    '/download' endpoint init

---
+++ src/api/routes/download.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator');
+const downloadController = require('../controllers/download');
+const is_auth = require('../middleware/is_auth');
+
+router.get(
+    '/download',
+    is_auth,
+    downloadController.streamer
+)
+
+module.exports = router;
